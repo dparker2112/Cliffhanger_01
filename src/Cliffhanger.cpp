cc42c3fd8c1ec5499cf2867c42847f5d12eef94e
dparker2112 <nlightn0@gmail.com>
2022-04-12 15:43:39 -0700

    working motion and sound - LNG

--- src/Cliffhanger.cpp
+++ src/Cliffhanger.cpp
@@ -1,56 +1,104 @@
 #include <Arduino.h>
-/*
-  Stepper Motor Demonstration 4
-  Stepper-Demo4.ino
-  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
+// Control for Cliffhanger game.  Made by David Parker nlightn0@gmail.com 04/01/2022
 
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
-  https://dronebotworkshop.com/stepper-motors-with-arduino/
-*/
+// this version uses millis() to manage timing rather than delay()
+// and the movement is determined by a pair of momentary push switches
+// press one and it turns CW, press the other and it turns CCW
 
-// Define Constants
+byte stepPin = 22;
+byte directionPin = 23;
 
-// Connections to A4988
-const int dirPin = 12;  // Direction
-const int stepPin = 13; // Step
+byte buttonCWpin = 4;
+byte buttonCCWpin = 5;
 
-// Motor steps per rotation
-const int STEPS_PER_REV = 200;
- 
-void setup() {
-  
-  // Setup the pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
+byte yodelPin = 30;
+byte fallPin = 31;
+
+byte startlocationPin = 24;
+byte falllocationPin = 25;
+
+boolean buttonCWpressed = false;
+boolean buttonCCWpressed = false;
+
+byte ledPin = 13;
+
+unsigned long curMillis;
+unsigned long prevStepMillis = 0;
+unsigned long millisBetweenSteps = 5; // milliseconds
+
+void setup() { 
+
+     Serial.begin(9600);
+     Serial.println("Starting Cliffhanger");
+
+     pinMode(directionPin, OUTPUT);
+     pinMode(stepPin, OUTPUT);
+     pinMode(ledPin, OUTPUT);
+     pinMode(yodelPin, OUTPUT);
+     pinMode(fallPin, OUTPUT);
+     
+     pinMode(buttonCWpin, INPUT_PULLUP);
+     pinMode(buttonCCWpin, INPUT_PULLUP);
+     pinMode(startlocationPin, INPUT_PULLUP);
+     pinMode(falllocationPin, INPUT_PULLUP);
+     
 }
-void loop() {
-  
-  // Set motor direction clockwise
-  digitalWrite(dirPin,HIGH); 
-  
-  // Spin motor one rotation slowly
-  for(int x = 0; x < (STEPS_PER_REV * 4); x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(2000); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(2000); 
-  }
-  
-  // Pause for one second
-  delay(3000); 
-  
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin,LOW);
-  
-  // Spin motor two rotations quickly
-  for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(1000);
-  }
-  
-  // Pause for one second
-  delay(3000);
+
+void loop() { 
+    
+    curMillis = millis();
+    readButtons();
+    actOnButtons();
+    playYodelSound();
+    playFallSound();
+}
+
+void readButtons() {
+    
+    buttonCCWpressed = false;
+    buttonCWpressed = false;
+    
+    if (digitalRead(buttonCWpin) == LOW) {
+        buttonCWpressed = true;
+    }
+    if (digitalRead(buttonCCWpin) == LOW) {
+        buttonCCWpressed = true;
+    }
+}
+
+void actOnButtons() {
+    if (buttonCWpressed == true) {
+        digitalWrite(directionPin, HIGH);
+        singleStep();
+    }
+    if (buttonCCWpressed == true) {
+        digitalWrite(directionPin, LOW);
+        singleStep();
+    }
+}
+
+void singleStep() {
+    if (curMillis - prevStepMillis >= millisBetweenSteps) {
+            // next 2 lines changed 28 Nov 2018
+        //prevStepMillis += millisBetweenSteps;
+        prevStepMillis = curMillis;
+        digitalWrite(stepPin, HIGH);
+        digitalWrite(stepPin, LOW);
+    }
+}
+void playYodelSound() {
+    if (buttonCWpressed == false) {
+        digitalWrite(yodelPin, HIGH);
+    }
+    if (buttonCWpressed == true) {
+        digitalWrite(yodelPin, LOW);
+    }
+}
+void playFallSound() {
+    if (buttonCWpressed == false) {
+        digitalWrite(fallPin, HIGH);
+    }
+    if (buttonCWpressed == true) {
+        digitalWrite(fallPin, HIGH);
+    }
 }
