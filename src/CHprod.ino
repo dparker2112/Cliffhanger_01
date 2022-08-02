58f25a3ac969cd0655c31b69d533f3ac8cd877fa
david <nlightn0@gmail.com>
2022-08-02 14:51:16 -0700

    sound buttons working

--- src/CHprod.ino
+++ src/CHprod.ino
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
 void setup() { 
-    pinMode(8, INPUT_PULLUP);                   //Cue 5 winning sound 1x
+    pinMode(8, INPUT_PULLUP);                   //Cue 4 winning sound 1x
+    pinMode(9, INPUT_PULLUP);                   //Cue 5 losing sound 1x
+    pinMode(10, INPUT_PULLUP);                  //Cue 6 idle music loop
     pinMode(33, OUTPUT);
+    pinMode(34, OUTPUT);
+    pinMode(35, OUTPUT);
 }
 
 void loop() {
     //read the buttons into variables
 
     //Cue #4, Sound track win, Button 4 > Pin 8 INPUT > Pin 33 OUTPUT > Pin 3 on sound board
-        int sensor5Val = digitalRead(8);
-        if (sensor5Val == HIGH) {
+        int sensor4Val = digitalRead(8);
+        if (sensor4Val == HIGH) {
             digitalWrite(33, HIGH);
         }
         else {
@@ -24,5 +28,14 @@ void loop() {
         }
         else {
             digitalWrite(34, LOW);
+    }
+
+    //Cue #6, Sound track idle music, Button 6 > Pin 10 INPUT > Pin 35 OUTPUT > Pin 6 on sound board
+        int sensor6Val = digitalRead(10);
+        if (sensor6Val == HIGH) {
+            digitalWrite(35, HIGH);
+        }
+        else {
+            digitalWrite(35, LOW);
     }
 }
