7c3a8f6fb154627d148dcc4a4ddf1332377bc10c
david <nlightn0@gmail.com>
2022-10-13 21:40:26 -0700

    return to home working

--- src/CHprod.ino
+++ src/CHprod.ino
@@ -44,16 +44,16 @@ T08.ogg                                 Reset Game Music
 */
 
 const int speed1 = 1000;
-const int speed2 = 500;
-
+const int speed2 = 250;
+//********************************LECTERN BUTTONS*********************************************
 const int reset = 4;                         //Cue 7 : Moves goat to Start of game location : resetPin 
 const int randomMove = 5;                    //Cue 1 : Moves goat random distance between resetPin and dangerPin
-const int space24 = 6;                       //Cue 2 : Moves goat to just before dangerPin
+const int space24 = 6;                       //Cue 2 : Moves goat to just before dangerPin (space 24)
 const int manual = 7;                        //Cue 3 : Moves goat until button is released
 const int win = 8;                           //Cue 4 : plays win sound
 const int lose = 9;                          //Cue 5 : plays lose sound
 const int idle = 10;                         //Cue 6 : plays win sound
-
+//********************************SOUND TRIGGERS**********************************************
 const int travelSoundPin = 30;               //pin 1 on sound board : Hold Looping Trigger
 const int fallSoundPin = 31;                 //pin 2 on sound board : Basic Trigger
 const int winSoundPin = 32;                  //pin 3 on sound board : Basic Trigger
@@ -61,20 +61,27 @@ const int dingSoundPin = 33;                 //pin 4 on sound board : Basic Trig
 const int loseSoundPin = 34;                 //pin 5 on sound board : Basic Trigger
 const int idleSoundPin = 35;                 //pin 6 on sound board : Latching Loop Trigger
 const int dangerSoundPin = 36;               //pin 7 on sound board : Basic Trigger
+//********************************SENSORS AND MOTOR CONTROL***********************************
+const int resetPin  = 24;                    //Start of game location
+const int dangerlocationPin = 25;            //Play Danger Sound
+const int fallPin = 26;                      //Play Lose Sound
 const int stepPin = 22;                      //Stepper Motor Control
 const int dirPin  = 23;                      //Stepper Motor Control
-const int resetPin  = 24;                    //Start of game location
+
 
 void setup() { 
     
-    pinMode(4, INPUT_PULLUP);
-    pinMode(5, INPUT_PULLUP);
-    pinMode(6, INPUT_PULLUP);
-    pinMode(7, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);                   //Cue 7 return to home position                    
+    pinMode(5, INPUT_PULLUP);                   //Cue 1 random move space 1 - 12
+    pinMode(6, INPUT_PULLUP);                   //Cue 2 go to space 24 
+    pinMode(7, INPUT_PULLUP);                   //Cue 3 manual move
     pinMode(8, INPUT_PULLUP);                   //Cue 4 winning sound 1x
     pinMode(9, INPUT_PULLUP);                   //Cue 5 losing sound 1x
     pinMode(10, INPUT_PULLUP);                  //Cue 6 idle music loop
-    
+    pinMode(24, INPUT_PULLUP);                  //optical sensor - home position
+    pinMode(25, INPUT_PULLUP);                  //optical sensor - triggers DANGER sound
+    pinMode(26, INPUT_PULLUP);                  //optical sensor - triggers LOSE sound
+
     pinMode(travelSoundPin, OUTPUT);
     pinMode(fallSoundPin, OUTPUT);
     pinMode(winSoundPin, OUTPUT);
@@ -99,6 +106,17 @@ void loop() {
 
     //read the buttons into variables
 
+    //Cue #7, Reset to Home Position, Button 7 > Pin 4 INPUT > STEP/DIR, No Music
+        int sensor7Val = digitalRead(4);
+        int sensor8Val = digitalRead(24);
+        if (sensor7Val == LOW && sensor8Val == HIGH) {
+            digitalWrite(dirPin,HIGH); // Enables the belt to move forward
+            digitalWrite(stepPin,HIGH);  
+            delayMicroseconds(speed2); 
+            digitalWrite(stepPin,LOW); 
+            delayMicroseconds(speed2);
+            }
+    
     /*
     //Cue #1, Random Move, Button 1 > Pin 5 INPUT > STEP/DIR, Travel music then Ding sound
         int sensor1Val = digitalRead(5);
@@ -112,11 +130,12 @@ void loop() {
     
     //Cue #2, Space 24 Move, Button 2 > Pin 6 INPUT > STEP/DIR, Travel music then Ding sound
         int sensor2Val = digitalRead(6);
-        if (sensor2Val == LOW) {
+        int sensor9Val = digitalRead(24);
+        if (sensor2Val == LOW && sensor9Val == LOW) {
             digitalWrite(travelSoundPin,LOW);
             digitalWrite(dirPin,HIGH); // Enables the belt to move forward
         // Makes 6100 pulses to go to space 24
-        for(int x = 0; x < 6100; x++) {
+        for(int x = 0; x < 6000; x++) {
             digitalWrite(stepPin,HIGH);  
             delayMicroseconds(speed1); 
             digitalWrite(stepPin,LOW); 
@@ -131,12 +150,12 @@ void loop() {
      //Cue #3, Manual Move, Button 3 > Pin 7 INPUT > STEP/DIR, Travel music then Ding sound
         int sensor3Val = digitalRead(7);
         if (sensor3Val == LOW) {
+            digitalWrite(travelSoundPin,LOW);
             digitalWrite(dirPin,HIGH); // Enables the belt to move forward
             digitalWrite(stepPin,HIGH);  
             delayMicroseconds(speed1); 
             digitalWrite(stepPin,LOW); 
             delayMicroseconds(speed1);
-            digitalWrite(travelSoundPin,LOW);
             digitalWrite(travelSoundPin,HIGH);
             digitalWrite(dingSoundPin,LOW);
             //delay(25);
